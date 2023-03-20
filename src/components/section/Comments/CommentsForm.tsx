63b7e9a6034e2eb069f2946b3b0d68409196073e
kai <kaienyeo225@gmail.com>
2023-03-21 00:43:47 +0800

    fix: comment form input, clean up

---
+++ src/components/section/Comments/CommentsForm.tsx
@@ -0,0 +1,54 @@
+import clsx from 'clsx'
+import React from 'react'
+import type { SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { useToastStore } from '@/stores/toast'
+import { useCommentsStore } from '@/stores/comment'
+
+interface Inputs {
+  name?: string
+  comment: string
+}
+
+function CommentsForm() {
+  const { formInputs, useCreateCommentMutation, presistForm } = useCommentsStore()
+  const { register, handleSubmit, reset, formState: { errors }, watch } = useForm<Inputs>({
+    defaultValues: {
+      ...formInputs.get(),
+    },
+  })
+  presistForm(watch())
+  const { notify } = useToastStore()
+  const createCommentMutation = useCreateCommentMutation()
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    createCommentMutation.mutate(formData, {
+      onSuccess() {
+        reset({
+          comment: '',
+        })
+        notify('Comment created')
+      },
+    })
+  }
+  const onError = () => {
+    notify('Comment is required')
+  }
+
+  return (
+    <form aria-disabled={createCommentMutation.isLoading} className='flex flex-col space-y-2 mx-auto max-w-[800px] border border-l-8 border-l-primary p-4' onSubmit={handleSubmit(onSubmit, onError)}>
+      <label className={clsx(errors.comment && 'error')}>
+        <span>Comment*</span>
+        <textarea disabled={createCommentMutation.isLoading} rows={3} {...register('comment', { required: true })} placeholder='Comment' />
+      </label>
+      <label>
+        <span>Name</span>
+        <input disabled={createCommentMutation.isLoading} {...register('name')} placeholder='Name' type="text" />
+      </label>
+      <button type='submit' className='button-primary lg:w-1/2' disabled={createCommentMutation.isLoading}>
+        {createCommentMutation.isLoading ? 'Loading' : 'Submit'}
+      </button>
+    </form>
+  )
+}
+
+export default CommentsForm
---
+++ src/components/section/Comments/CommentsList.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef } from 'react'
+import { useCommentsStore } from '@/stores/comment'
+import { useToastStore } from '@/stores/toast'
+
+interface Props {}
+
+const CommentsList = (props: Props) => {
+  const { useCommentsQuery } = useCommentsStore()
+  const { notify } = useToastStore()
+  const { data, hasNextPage, fetchNextPage } = useCommentsQuery()
+
+  const lastCommentRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+      if (isIntersecting && hasNextPage) {
+        notify('Loading more comments...')
+        fetchNextPage()
+      }
+      if (isIntersecting && !hasNextPage)
+        notify('You have reached the end')
+    })
+    observer.observe(lastCommentRef.current!)
+    return () => {
+      observer.disconnect()
+    }
+  }, [hasNextPage])
+  return (
+    <div className='flex flex-col gap-y-2 border-l-2 border-l-primary py-2 pl-1 relative'>
+      {(data?.pages !== undefined && data.pages.length !== 0)
+        ? data?.pages.map(page => (
+          page.commentsConnection.edges.map((c, i) => (
+            <CommentItem {...c.node} key={c.node.id}></CommentItem>
+          ))
+        ))
+        : <div className='px-2 text-primary/50'>No comments currently...</div>
+      }
+      <div ref={lastCommentRef} className='bg-red-200 absolute bottom-0 -z-10 opacity-0'>last</div>
+    </div>
+  )
+}
+
+export default CommentsList
+
+interface CommentItemData {
+  id: string
+  name?: string | null | undefined
+  comment?: string | null | undefined
+  createdAt: string
+}
+function CommentItem({ name, comment, createdAt }: CommentItemData) {
+  const date = new Date(createdAt)
+  return (
+    <div className='w-full border p-2 bg-background'>
+      <div className='flex gap-5 items-center'>
+        <div className='text-2xl text-bold'>
+          {name}
+        </div>
+        <div className='text-primary/70'>
+          {date.toLocaleTimeString()} | {date.toDateString()}
+        </div>
+      </div>
+      <div className='p-2 border-t border-t-primary/10'>
+        {comment}
+      </div>
+    </div>
+  )
+}
