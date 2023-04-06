4f541135ff0da8fc8830abe5315bf9e7dab12850
kai <kaienyeo225@gmail.com>
2023-04-06 18:49:01 +0800

    style: 1280 wide responsive

--- src/layouts/SectionLayout.tsx
+++ src/layouts/SectionLayout.tsx
@@ -26,7 +26,7 @@ const SectionLayout: React.FC<PropsWithChildren> = ({ children }) => {
   }, [])
 
   return (
-    <section id='section' className='relative h-screen w-screen flex items-center justify-center md:px-52 z-10 snap-center'>
+    <section id='section' className='relative h-screen w-screen flex items-center justify-center lg:px-36 xl:px-52 z-10 snap-center'>
       <DesktopNavbar></DesktopNavbar>
       <SwitchTransition>
         <CSSTransition key={sectionTitle} nodeRef={bodyRef} classNames='expand' addEndListener={(done) => { bodyRef.current?.addEventListener('transitionend', done, false) }}>
@@ -52,7 +52,7 @@ const SectionLayout: React.FC<PropsWithChildren> = ({ children }) => {
         </CSSTransition>
       </SwitchTransition>
       <div className='bg-dotted h-64 w-64 absolute bottom-10 right-5'></div>
-      <div className='absolute md:left-6 md:bottom-6 bottom-6 flex gap-5'>
+      <div className='absolute xl:left-6 bottom-6 flex gap-5'>
         <div className='rounded-full bg-[rgb(34,255,254)] w-5 h-5'></div>
         <div className='rounded-full bg-[rgb(253,35,249)] w-5 h-5'></div>
         <div className='rounded-full bg-[rgb(255,253,64)] w-5 h-5'></div>
